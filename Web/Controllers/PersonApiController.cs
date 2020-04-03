1aff3fd60dcc69fd07c5b03617c90f7e4f66462f
Denis Malimon <malimon88@gmail.com>
2020-04-03 20:29:41 +0300

    Example: create content using Umbraco services

---
+++ Web/Controllers/PersonApiController.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http;
+using Umbraco.Web.PublishedModels;
+using Umbraco.Web.WebApi;
+
+namespace Web.Controllers
+{
+    public class PersonApiController : UmbracoApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Create()
+        {
+            var rootNode = Umbraco.ContentAtRoot().OfType<People>().FirstOrDefault();
+
+            var content = Services.ContentService.Create("Person1", rootNode.Id, Supplier.ModelTypeAlias);
+            content.SetValue(Supplier.GetModelPropertyType(x => x.PersoneName).Alias, "Created from code");
+            content.SetValue(Supplier.GetModelPropertyType(x => x.Phone).Alias, "80979219600");
+            content.SetValue(Supplier.GetModelPropertyType(x => x.Message).Alias, "80979219600");
+
+            var result = Services.ContentService.Save(content);
+            return Ok();
+        }
+    }
+}
