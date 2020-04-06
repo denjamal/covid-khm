9dfc7351dc9c0d618543f2558a8ecfb3f1a6788b
Danil Danilchenko <danildanilchenko.27@gmail.com>
2020-04-06 12:41:58 +0300

    Added registration form and controllers

---
+++ Web/App_Data/Models/VolunteerDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.App_Data.Models
+{
+    public class VolunteerDTO
+    {
+        public string PersoneName { get; set; }
+        public string Phone { get; set; }
+        public string HomeAddress { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+    }
+}
