480a80562a1224cdc44247b47a8da7674063bae5
andrey94pushkar <andrey94pushkar@gmail.com>
2020-04-06 17:02:32 +0300

    Sending contact form.

---
+++ Web/Models/ContactDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models
+{
+    public class ContactDTO
+    {
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactMessage { get; set; }
+        public string ContactCategory { get; set; }
+    }
+}
