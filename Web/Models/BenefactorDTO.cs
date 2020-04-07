82fedaa3826afeeb5741cb52bfbba32d55534859
Denis Malimon <malimon88@gmail.com>
2020-04-07 10:38:00 +0300

    Small changes

---
+++ Web/Models/BenefactorDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models
+{
+    public class BenefactorDTO
+    {
+        public string PersonName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
