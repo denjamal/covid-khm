2eaecf44875c1236d4eee1f8bf0ad079f8dd2155
Denis Malimon <malimon88@gmail.com>
2020-04-09 10:44:05 +0300

    Add form validation

--- Web/assets/js/signUp.js
+++ Web/assets/js/signUp.js
@@ -1,61 +1,64 @@
 ﻿
+var url;
+var requiredFieldMsg = "Це поле обов'язкове";
+
 jQuery(document).ready(function () {
-    $('#form').submit(function () {
-        if (document.form.PersonName == '' || document.form.Phone == '' || document.form.type.value=="type_0") {
-            valid = false;
-            return valid;
+    var form$ = $("#form");
+
+    form$.validate({
+        rules: {
+            personName: "required",
+            phone: "required",
+            category: 'required'
+        },
+        messages: {
+            personName: {
+                required: requiredFieldMsg
+            },
+            phone: {
+                required: requiredFieldMsg
+            },
+            category: {
+                required: requiredFieldMsg
+            }
+        },
+        submitHandler: function () {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: form$.serialize()
+            }).done(function () {
+                alert("Заявка успішно надіслана");
+                form$.trigger('reset');
+            });
         }
-        $.ajax({
-            type: "POST",
-            url: __url,
-            data: $(this).serialize()
-        }).done(function () {
-            $(this).find('input').val('');
-            $('#form').trigger('reset');
-        });
-        return false;
     });
 });
 
-let __url;
 
-function TypeChange() {
-    if (document.getElementById('type').value == "type_2") {
-        // document.getElementById('form1').action = "/umbraco/api/signup/createVolunteer";
-        __url = "/umbraco/api/signup/createVolunteer";
-        document.getElementById('email').style.display = "none";
-        document.getElementById('address').style.display = "block";
-        document.getElementById('age').style.display = "block";
-        document.getElementById('description').placeHolder = "Description";
+function formTypeChanged() {
+    if (document.getElementById('type').value === "type_2") {
+        url = "/umbraco/api/signup/createVolunteer";
+        $('#email').hide();
+        $('#address').show();
+        $('#age').show();
     }
-    else if (document.getElementById('type').value == "type_3") {
-        // document.getElementById('form1').action = "/umbraco/api/signup/createSupplier";
-        __url = "/umbraco/api/signup/createSupplier";
-        document.getElementById('email').style.display = "block";
-        document.getElementById('address').style.display = "none";
-        document.getElementById('age').style.display = "none";
-        document.getElementById('description').placeHolder = "Description";
+    else if (document.getElementById('type').value === "type_3") {
+        url = "/umbraco/api/signup/createSupplier";
+        $('#email').show();
+        $('#address').hide();
+        $('#age').hide();
     }
-    else if (document.getElementById('type').value == "type_4") {
-        //document.getElementById('form1').action = "/umbraco/api/signup/createBenefactor";
-        __url = "/umbraco/api/signup/createBenefactor";
-        document.getElementById('email').style.display = "block";
-        document.getElementById('address').style.display = "none";
-        document.getElementById('age').style.display = "none";
-        document.getElementById('description').placeHolder = "Description";
+    else if (document.getElementById('type').value === "type_4") {
+        url = "/umbraco/api/signup/createBenefactor";
+        $('#email').show();
+        $('#address').hide();
+        $('#age').hide();
     }
-    else if (document.getElementById('type').value == "type_1") {
-        // document.getElementById('form1').action = "/umbraco/api/signup/CreateExtraDoctor";
-        __url = "/umbraco/api/signup/CreateExtraDoctor";
-        document.getElementById('email').style.display = "none";
-        document.getElementById('address').style.display = "block";
-        document.getElementById('age').style.display = "block";
-        document.getElementById('description').placeHolder = "Medical specialization";
-    }
-    else if (document.getElementById('type').value == "type_0") {
-        document.getElementById('email').style.display = "block";
-        document.getElementById('address').style.display = "block";
-        document.getElementById('age').style.display = "block";
-        document.getElementById('description').placeHolder = "Description";
+    else if (document.getElementById('type').value === "type_1") {
+        url = "/umbraco/api/signup/CreateExtraDoctor";
+        $('#email').hide();
+        $('#address').show();
+        $('#age').show();
     }
 }
